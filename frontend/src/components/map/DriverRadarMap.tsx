fb0b35b1cb0f63615b581522a01f5355ed619ecb
Omkarchavan2527 <omkarchavan2725@gmail.com>
2026-04-13 08:04:46 +0530

    Update DriverRadarMap.tsx

--- frontend/src/components/map/DriverRadarMap.tsx
+++ frontend/src/components/map/DriverRadarMap.tsx
@@ -9,7 +9,7 @@ const driverIcon = L.divIcon({
   html: `
     <div class="relative flex items-center justify-center w-6 h-6">
       <div class="absolute inset-0 bg-driver rounded-full radar-ring"></div>
-      <div class="w-4 h-4 bg-driver rounded-full shadow-[0_0_10px_#22C55E] z-10 border-2 border-[#0F1117]"></div>
+      <div class="w-4 h-4 bg-driver rounded-full shadow-[0_0_10px_#22C55E] z-10 border-2 border-white"></div>
     </div>
   `,
   iconSize: [24, 24],
@@ -20,7 +20,7 @@ const driverIcon = L.divIcon({
 const userIcon = L.divIcon({
   className: 'custom-user-marker',
   html: `
-    <div class="w-5 h-5 bg-yellow-500 rounded-full border-2 border-[#0F1117] shadow-sm flex items-center justify-center">
+    <div class="w-5 h-5 bg-yellow-500 rounded-full border-2 border-white shadow-sm flex items-center justify-center">
       <div class="w-2 h-2 bg-white rounded-full"></div>
     </div>
   `,
@@ -50,17 +50,16 @@ export const DriverRadarMap: React.FC = () => {
   }, []);
 
   return (
-    <div className="absolute inset-0 z-0 bg-[#0F1117]">
+    <div className="absolute inset-0 z-0 bg-gray-100">
       <MapContainer 
         center={driverPosition} 
         zoom={15} 
         zoomControl={false} 
         className="w-full h-full"
       >
-        {/* CartoDB Dark Matter Tiles */}
         <TileLayer
-          attribution='&copy; OpenStreetMap &copy; CARTO'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           maxZoom={19}
         />
         
